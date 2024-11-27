6e0a60d4cf305cfd31612451a8378845b3df8963
userName <user@gmail.com>
2024-11-27 11:44:29 +0530

    Initial commit

---
+++ src/main/java/com/vrv/vrvsecurityassignment/VrvSecurityAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.vrv.vrvsecurityassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrvSecurityAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrvSecurityAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrv/vrvsecurityassignment/VrvSecurityAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrv.vrvsecurityassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrvSecurityAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
